702b79569fa9cf04234a4b9b94ff807c802091f1
Hans Hjelm <hans.e.hjelm@gmail.com>
2017-03-27 14:24:32 +0200

    add setup.py, adapt to python3, change normalization to work for unicode

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hyperwords',
+      version='1.0',
+      description='Word space models reproducing sgns with PPMI and SVD',
+      author='Omer Levy',
+      author_email='omerlevy@cs.biu.ac.il',
+      url='https://bitbucket.org/omerlevy/hyperwords',
+      packages=['hyperwords'],
+)
